5d5c048decce8799f2218ce1488aef2886c6aa2e
Lucas Berardi <lucas-l-@live.com>
2023-09-04 01:27:40 -0300

    app.js done

---
+++ Challenge-4.5-complementary/src/app.js
@@ -0,0 +1,18 @@
+import express  from "express";
+import productRouter from "./routes/products.routes.js";
+import mongoose from "mongoose";
+
+const app = express()
+const PORT = 4000
+
+mongoose.connect('mongodb+srv://lucasberardi17:lucas1234@cluster0.rjyqxv4.mongodb.net/?retryWrites=true&w=majority')
+.then(()=> console.log('DB connected'))
+.catch((error)=> console.log(`Error connecting to MongoDB Atlas: ${error}`))
+
+app.use(express.json())
+
+app.use('/api/products', productRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Server on port ${PORT}`);
+})
