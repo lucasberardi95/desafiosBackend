f7205fded417971dcb82d735e62afb38263355eb
Lucas Berardi <lucas-l-@live.com>
2023-09-05 02:43:03 -0300

    Carts routes added

--- Challenge-4.5-complementary/src/app.js
+++ Challenge-4.5-complementary/src/app.js
@@ -1,5 +1,6 @@
 import express  from "express";
 import productRouter from "./routes/products.routes.js";
+import cartRouter from "./routes/carts.routes.js";
 import mongoose from "mongoose";
 
 const app = express()
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://lucasberardi17:lucas1234@cluster0.rjyqxv4.mongod
 app.use(express.json())
 
 app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Server on port ${PORT}`);
