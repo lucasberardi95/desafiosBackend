420218a1eb86fa34b9007142c54cd4f6a6b3af66
Lucas Berardi <lucas-l-@live.com>
2023-09-06 04:00:14 -0300

    Socket.io connection

--- Challenge-4.5-complementary/src/app.js
+++ Challenge-4.5-complementary/src/app.js
@@ -7,9 +7,30 @@ import mongoose from "mongoose"
 import productRouter from "./routes/products.routes.js"
 import cartRouter from "./routes/carts.routes.js"
 import messageRouter from "./routes/messages.routes.js"
+import messageModel from "./models/messages.models.js"
 
 const app = express()
 const PORT = 4000
+
+//Server
+const server = app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
+const io = new Server(server)
+
+//Conexion de Socket.io
+io.on('connection', (socket)=>{
+    console.log('Socket.io connection')
+    socket.on('message', async info =>{
+        const {email, message} = info
+        await messageModel.create({
+            email,
+            message
+        })
+        const messages = await messageModel.find()
+        io.emit('messages', messages)
+    })
+})
 
 //MongoDB Atlas connection
 mongoose.connect('mongodb+srv://lucasberardi17:lucas1234@cluster0.rjyqxv4.mongodb.net/?retryWrites=true&w=majority')
@@ -24,14 +45,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static(path.join(__dirname, '/public')))
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
-app.use('/', messageRouter)
+app.use('/api/messages', messageRouter)
+
+app.get('/static', (req,res)=>{
+    res.render('chat',{
+        rutaCSS: 'chat',
+        rutaJS: 'chat',
+    })
+})
 
 //HBS configuration
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', path.resolve(__dirname, 'views'))
-
-
-app.listen(PORT, ()=>{
-    console.log(`Server on port ${PORT}`)
-})
