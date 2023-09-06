aefb1e22f2971e8150ba824159a08d1a82801579
Lucas Berardi <lucas-l-@live.com>
2023-09-06 02:51:58 -0300

    Sintax fix

--- Challenge-4.5-complementary/src/routes/products.routes.js
+++ Challenge-4.5-complementary/src/routes/products.routes.js
@@ -1,5 +1,5 @@
-import { Router } from "express";
-import productModel from "../models/products.models.js";
+import { Router } from "express"
+import productModel from "../models/products.models.js"
 
 const productRouter = Router()
 
