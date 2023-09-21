d2616177b5d8a7c5e8ee70f85398cb8678c97dd8
Lucas Berardi <lucas-l-@live.com>
2023-09-21 03:11:53 -0300

    Syntax fix

--- Challenge-5/src/routes/users.routes.js
+++ Challenge-5/src/routes/users.routes.js
@@ -6,8 +6,9 @@ const userRouter = Router()
 userRouter.post('/signin', async (req, res)=>{
     const {first_name, last_name, email, password, age} = req.body
     try {
-        const response = await userModel.create({first_name, last_name, email, password, age})
-        res.status(200).send({ result: 'Registered user', message: response})
+        const user = await userModel.create({first_name, last_name, email, password, age})
+        //res.status(200).send({ result: 'Registered user', message: response})
+        res.redirect(`/static/products?info=${user.first_name}`) //Redirect
     } catch (error) {
         res.status(400).send({ error: `Error creating user ${error}`})
     }
