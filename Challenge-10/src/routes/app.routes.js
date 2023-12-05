ac1d69e81876457b5d0f67bd836117a6fdb52658
Lucas Berardi <lucas-l-@live.com>
2023-12-05 04:03:57 -0300

    Updated API doc with swagger

--- Challenge-10/src/routes/app.routes.js
+++ Challenge-10/src/routes/app.routes.js
@@ -7,6 +7,8 @@ import userRouter from './users.routes.js'
 import sessionRouter from './sessions.routes.js'
 import viewRouter from "./views.routes.js"
 import loggerRouter from "./loggers.routes.js"
+import swaggerUiExpress from 'swagger-ui-express'
+import { specs } from "../config/swagger.js"
 
 const router = Router()
 
@@ -18,5 +20,6 @@ router.use('/api/mockingproducts', routerMock)
 router.use('/api/users', userRouter)
 router.use('/api/sessions', sessionRouter)
 router.use('/', viewRouter)
+router.use('/apidocs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
 
 export default router
