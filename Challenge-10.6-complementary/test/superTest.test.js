e40ed7561969e8e86c75e2d65fceecc2a5686d0f
Lucas Berardi <lucas-l-@live.com>
2023-12-20 15:13:41 -0300

    Register, login & add product fixes on superTest

--- Challenge-10.6-complementary/test/superTest.test.js
+++ Challenge-10.6-complementary/test/superTest.test.js
@@ -3,6 +3,8 @@ import chai from 'chai'
 import supertest from 'supertest'
 import mongoose from 'mongoose'
 import { logger } from '../src/utils/logger.js'
+import { userModel } from '../src/models/users.models.js'
+import productModel from '../src/models/products.models.js'
 
 await mongoose
     .connect(process.env.MONGO_URL)
@@ -29,16 +31,20 @@ describe('App tests', () => {
     it('Endpoint test /api/users/signin, a new user is expected to be created', async function () {
         this.timeout(7000)
 
-        const { __body, status } = await requester.post('/api/users/signin').send(newUser)
+        const { status } = await requester
+        .post('/api/users/signin')
+        .send(newUser)
+        const user = await userModel.findOne({ email: newUser.email });
 
         if (status === 302) {
             //User created succesfully
             expect(status).to.equal(302) // Código 302 porque al registrarse hace una redirección a la vista de inicio de sesión
-            logger.info(`Status: ${__body}`)
+            logger.info(`User created succesfully. Status: ${status}`)
+            logger.info(`User: ${user}`)
         } else if (status === 401) {
             //User already exists
             expect(status).to.equal(401)
-            logger.info(`User already exists: ${__body}`)
+            logger.info(`User already exists. Status: ${status}`)
         } else {
             //Any other unexpected status
             throw new Error(`Unexpected status code: ${status}`)
@@ -48,13 +54,15 @@ describe('App tests', () => {
     it('Endpoint test /api/sessions/login, a user is expected to log in', async function () {
         this.timeout(7000)
 
-        const response = await requester.post('/api/sessions/login').send(newUser);
+        const response = await requester
+        .post('/api/sessions/login')
+        .send(newUser);
         const { __body } = response;
         const tokenResult = response.header['set-cookie'][0];
 
         expect(tokenResult).to.be.ok;
         expect(response.status).to.be.equal(302);//status(302) de nuevo por ser redirect
-
+        logger.info('Login status: ', response.status);
         token = {
             name: tokenResult.split('=')[0],
             value: tokenResult.split('=')[1]
@@ -62,9 +70,11 @@ describe('App tests', () => {
 
         expect(token).to.be.ok;
         expect(token.name).to.be.ok.and.equal('jwtCookie');
-        expect(__body.cartId).to.be.ok
-        userId = __body._id
-        cartId = __body.cartId
+
+        const user = await userModel.findOne({ email: newUser.email })
+        logger.info(user)
+        userId = user._id
+        cartId = user.cart._id
         logger.info(`Token: ${token.name} = ${token.value}`); 
     });
 
@@ -78,22 +88,44 @@ describe('App tests', () => {
 
     it('Endpoint test /api/carts/cid/product/pid, a product is expected to be added to the cart', async function () {
         const cid = cartId
-        const pid = ''
+        const newProduct = await productModel.create({
+            title:"test product",
+            description:"this is a test product",
+            price:100,
+            stock:500,
+            category:"testing",
+            code:`test${Math.random().toString(36).substring(7)}`
+        })
+        const pid = newProduct._id
+        const quantity = 1 
+        console.log('Cart ID:', cid);
+        console.log('Product ID:', pid);
+        console.log('Token:', token);
 
         /* await requester.post(`/api/carts/${cid}/product/${pid}`).set('Cookie', [`${token.name} = ${token.value}`]) TO ADD TWICE THE SAME PRODUCT */ 
-        const { __body, status } = await requester.post(`/api/carts/${cid}/product/${pid}`).set('Cookie', [`${token.name} = ${token.value}`])
+        const { __body, status } = await requester
+        .put(`/api/carts/${cid}/product/${pid}`)
+        .send({ quantity })
+        .set('Cookie', [`${token.name} = ${token.value}`])
+
+        console.log('Response Status:', status);
+        console.log('Response Body:', __body);
 
         expect(status).to.equal(200)
+
         logger.info('Product added succesfully')
-        logger.info(`Product: ${__body.products}`)
+        logger.info(`Product: ${__body}`)
     })
 
-    it('Endpoint test /api/carts/cid/product/pid, it is expected to modify the quantity of a product in the cart', async function () {
-        const cid = cartId
-        const pid = ''
-        const newQty = { quantity : 10 }
+    it('Endpoint test /api/carts/cid/products/pid, it is expected to modify the quantity of a product in the cart', async function () {
+        const cid = '6582f9ce41fda5676222140a'
+        const pid = '6582ffb63b1b5467d598efb5'
+        const newQty = { quantity : 20 }
         
-        const { __body, status } = await requester.put(`/api/carts/${cid}/product/${pid}`).send(newQty).set('Cookie', [`${token.name} = ${token.value}`])
+        const { __body, status } = await requester
+        .put(`/api/carts/${cid}/products/${pid}`)
+        .send(newQty)
+        .set('Cookie', [`${token.name} = ${token.value}`])
 
         expect(status).to.equal(200)
         logger.info('Updated product qty')
